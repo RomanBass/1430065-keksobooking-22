f668e4c2344cc9a28ef37912b8b832965d092d5f
RomanBass <49683918+RomanBass@users.noreply.github.com>
2021-02-06 21:34:28 +0300

    на проверку

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 'use strict';
 
+const ESTATE_OBJECTS_NUMBER = 10;
+const AUTHORS_MAX_NUMBER = 8;
+const CHECK_IN_OUT = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const DESCRIPTIONS = ['Уютное место, рядом вокзал', 'Тихое место, рядом казино.', 'Соседи повышенного уровня культуры, рядом прачечная.', 'Для постояльцев с IQ не выше 90.'];
+const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg', 'https://i2.wp.com/www.sonmezrealestate.com/wp-content/uploads/2018/05/080-1.jpg?fit=1688%2C1125&ssl=1', 'https://elets-adm.ru/assets/images/resources/4281/c50acc2e5c569e61107070cc7fdb5ac0ae840194.jpg'];
+
+const TITLES = ['Замок с приведениями.', 'В тесноте, но не в обиде.', 'Царские покои.', 'Хрущёвка ХХI века.'];
+const LocationRange = {
+  X_MIN: 35.65000,
+  X_MAX: 35.70000,
+  Y_MIN: 139.70000,
+  Y_MAX: 139.80000,
+  SIGNS_NUMBER: 5,
+};
+const PriceRange = {
+  MIN: 1000,
+  MAX: 10000,
+  PRECISION: 100,
+};
+const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const RoomsNumberRange = {
+  MIN: 0,
+  MAX: 30,
+};
+const GuestsNumberRange = {
+  MIN: 1,
+  MAX: 100,
+}
+
 const getRandomInteger = function (min, max) {
   if (min < 0 || max < 0) {
     alert('Параметры не могут быть отрицательными');
@@ -40,45 +70,39 @@ const getRandomFloat = function (min, max, precision) {
   return randomScaled / 10 ** precision;
 };
 
-getRandomInteger(5, 63);
-getRandomFloat(0.123, 985.12, 2);
-
-const MAX_AUTHORS_NUMBER = 8;
-const TITLES = ['Замок с приведениями.', 'В тесноте, но не в обиде.', 'Царские покои.', 'Хрущёвка ХХI века.'];
-const LocationRange = {
-  X_MIN: 35.65000,
-  X_MAX: 35.70000,
-  Y_MIN: 139.70000,
-  Y_MAX: 139.80000,
-  PRECISION_SIGNS_NUMBER: 5,
+const reduceAndRandomizeArray = function (array) {
+  let tinyArray = [];
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  for (let k = 0; k < getRandomInteger(0, array.length); k++) {
+    tinyArray[k] = array[k];
+  }
+  return tinyArray;
 };
-const PriceRange = {
-  PRICE_MIN: 1000,
-  PRICE_MAX: 10000,
-  PRICE_PRECISION: 100,
-}
 
-let getEstateObject = function () {
-
-  const locationX = getRandomFloat(LocationRange.X_MIN, LocationRange.X_MAX, LocationRange.PRECISION_SIGNS_NUMBER);
-  const locationY = getRandomFloat(LocationRange.Y_MIN, LocationRange.Y_MAX, LocationRange.PRECISION_SIGNS_NUMBER);
+const createEstateObject = function () {
+  const locationX = getRandomFloat(LocationRange.X_MIN, LocationRange.X_MAX, LocationRange.SIGNS_NUMBER);
+  const locationY = getRandomFloat(LocationRange.Y_MIN, LocationRange.Y_MAX, LocationRange.SIGNS_NUMBER);
+  const checkInOut = CHECK_IN_OUT[getRandomInteger(0, CHECK_IN_OUT.length - 1)];
 
   return {
     author: {
-      avatar:`img/avatars/user0${getRandomInteger(1, MAX_AUTHORS_NUMBER)}.png`,
+      avatar:`img/avatars/user0${getRandomInteger(1, AUTHORS_MAX_NUMBER)}.png`,
     },
     offer: {
       title: TITLES[getRandomInteger(0, TITLES.length - 1)],
       address: `${locationX}, ${locationY}`,
-      price: getRandomFloat(PriceRange.PRICE_MIN, PriceRange.PRICE_MAX, - Math.log10(PriceRange.PRICE_PRECISION)),
-      type: [],
-      rooms: [],
-      guests: [],
-      checkin: [],
-      checkout: [],
-      features: [],
-      description: [],
-      photos: [],
+      price: getRandomFloat(PriceRange.MIN, PriceRange.MAX, - Math.log10(PriceRange.PRECISION)),
+      type: TYPES[getRandomInteger(0, TYPES.length - 1)],
+      rooms: getRandomInteger(RoomsNumberRange.MIN, RoomsNumberRange.MAX),
+      guests: getRandomInteger(GuestsNumberRange.MIN, GuestsNumberRange.MAX),
+      checkin: checkInOut,
+      checkout: checkInOut,
+      features: reduceAndRandomizeArray(FEATURES),
+      description: DESCRIPTIONS[getRandomInteger(0, DESCRIPTIONS.length - 1)],
+      photos: reduceAndRandomizeArray(PHOTOS),
     },
     location: {
       x: locationX,
@@ -87,31 +111,10 @@ let getEstateObject = function () {
   }
 };
 
-console.log(getEstateObject());
+const estateObjects = new Array(ESTATE_OBJECTS_NUMBER).fill(null).map(function() {
+  return createEstateObject();
+});
 
-// В файле main.js на основе написанных в прошлом задании утилитарных функций напишите необходимые функции для создания
-// массива из 10 сгенерированных JS-объектов. Каждый объект массива — описание похожего объявления неподалёку.
-
-// Структура каждого объекта должна быть следующей:
-
-// author, объект — описывает автора. Содержит одно поле:
-// avatar, строка — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это случайное число от 1 до 8 с ведущим нулём.
-// Например, 01, 02 и т. д.
-
-// offer, объект — содержит информацию об объявлении. Состоит из полей:
-// title, строка — заголовок предложения. Придумайте самостоятельно.
-// address, строка — адрес предложения. Для простоты пусть пока составляется из географических координат по маске {{location.x}}, {{location.y}}.
-// price, число — стоимость. Любое положительное число.
-// type, строка — одно из четырёх фиксированных значений: palace, flat, house или bungalow.
-// rooms, число — количество комнат. Любое положительное число.
-// guests, число — количество гостей, которое можно разместить. Любое положительное число.
-// checkin, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-// checkout, строка — одно из трёх фиксированных значений: 12:00, 13:00 или 14:00.
-// features, массив строк — массив случайной длины из значений: wifi, dishwasher, parking, washer, elevator, conditioner. Значения не должны повторяться.
-// description, строка — описание помещения. Придумайте самостоятельно.
-// photos, массив строк — массив случайной длины из значений: http://o0.github.io/assets/images/tokyo/hotel1.jpg, http://o0.github.io/assets/images/tokyo/hotel2.jpg, http://o0.github.io/assets/images/tokyo/hotel3.jpg.
-
-// location, объект — местоположение в виде географических координат. Состоит из двух полей:
-// x, число с плавающей точкой — широта, случайное значение от 35.65000 до 35.70000
-// y, число с плавающей точкой — долгота, случайное значение от 139.70000 до 139.80000
+estateObjects;
+// console.log(estateObjects); //закомментил, чтобы линт не ругался на console.log
 
