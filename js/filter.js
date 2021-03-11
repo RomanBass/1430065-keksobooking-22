73c016bb765c542dec7458eb0977d4ae0a1231e0
Roman Katrovsky <katrovsky@gmail.com>
2021-03-11 21:46:56 +0300

    for the 2nd review

--- js/filter.js
+++ js/filter.js
@@ -13,69 +13,73 @@ const switchFiltersActivation = (deactivator) => { // деактивируетс
   });
 }
 
-const changeFiltersState = (cb, dataToFilter) => { // отрисовка меток с учётом применения фильтров
-  filterForm.addEventListener('change', () => {
+const checkHousingType = (estateObject) => { // проверка совпадения estateObject-та с выбранным типом жилья
+  const housingType = document.querySelector('#housing-type option:checked').value; // текущий параметр типа жилья
+  if (housingType === estateObject.offer.type || housingType === 'any') {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-    const housingType = document.querySelector('#housing-type option:checked').value; // текущий параметр типа жилья
-    const housingPrice = document.querySelector('#housing-price option:checked').value; // текущий параметр диапазона цен из селектора
-    const housingRooms = document.querySelector('#housing-rooms option:checked').value; // текущий параметр количества комнат
-    const housingGuests = document.querySelector('#housing-guests option:checked').value; // текущий параметр количества гостей
-    const facilitiesArray = document.querySelectorAll('#housing-features input'); // массив опций удобств
+const checkPriceRange = (estateObject) => { // проверка попадания estateObject-та в выбранный диапазон цен
+  const housingPrice = document.querySelector('#housing-price option:checked').value; // текущий параметр диапазона цен из селектора
+  let indicator = false;
+  switch (housingPrice) {
+    case 'any':
+      indicator = true;
+      break;
+    case 'low':
+      indicator = (estateObject.offer.price <= 10000);
+      break;
+    case 'middle':
+      indicator = (estateObject.offer.price > 10000 && estateObject.offer.price < 50000);
+      break;
+    case 'high':
+      indicator = (estateObject.offer.price >= 50000);
+  }
+  return indicator;
+};
 
-    const housingTypeFilteredOffers = dataToFilter.filter((estateObject) => { // фильтрация данных с сервера по типу жилья
-      if (housingType === 'any') {
-        return dataToFilter;
-      } else {
-        return estateObject.offer.type === housingType;
-      }
-    });
+const checkHousingRooms = (estateObject) => { // проверка в estateObject-те выбранного количества комнат
+  const housingRooms = document.querySelector('#housing-rooms option:checked').value; // текущий параметр количества комнат
+  if (estateObject.offer.rooms === parseInt(housingRooms, 10) || housingRooms === 'any') {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-    const housingPriceFilteredOffers = housingTypeFilteredOffers.filter(function (estateObject) { // фильтрация данных из предыдущего фильтра по диапазону цены
-      let priceRange;
-      switch (housingPrice) {
-        case 'any':
-          priceRange = housingTypeFilteredOffers;
-          break;
-        case 'low':
-          priceRange = (estateObject.offer.price <= 10000);
-          break;
-        case 'middle':
-          priceRange = (estateObject.offer.price > 10000 && estateObject.offer.price < 50000);
-          break;
-        case 'high':
-          priceRange = (estateObject.offer.price >= 50000);
-      }
-      return priceRange;
-    });
+const checkHousingGuests = (estateObject) => { // проверка в estateObject-те выбранного количества гостей
+  const housingGuests = document.querySelector('#housing-guests option:checked').value; // текущий параметр количества гостей
+  if (estateObject.offer.guests === parseInt(housingGuests, 10) || housingGuests === 'any') {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-    const housingRoomsFilteredOffers = housingPriceFilteredOffers.filter(function (estateObject) { // фильтрация данных из предыдущего фильтра по количеству комнат
-      if (housingRooms === 'any') {
-        return housingPriceFilteredOffers;
-      } else {
-        return estateObject.offer.rooms === parseInt(housingRooms, 10); // преобразуем строку в целое число, т.к. value содержит строковый параметр
-      }
-    });
-
-    const housingGuestsFilteredOffers = housingRoomsFilteredOffers.filter(function (estateObject) { // фильтрация данных из предыдущего фильтра по количеству гостей
-      if (housingGuests === 'any') {
-        return housingRoomsFilteredOffers;
-      } else {
-        return estateObject.offer.guests === parseInt(housingGuests, 10); // преобразуем строку в целое число, т.к. value содержит строковый параметр
-      }
-    });
-
-    let facilitiesFilteredOffers = housingGuestsFilteredOffers; // создаём массив предложений, который далее будем фильтровать по критерию удобств
-
-    for (let i = 0; i < facilitiesArray.length; i++) { // цикл проходит по всем элементам массива опций удобств
-      if (facilitiesArray[i].checked) { // если удобство i чекнуто, то...
-        facilitiesFilteredOffers = facilitiesFilteredOffers.filter(function (estateObject) { // фильтруем массив по удобству i
-          return estateObject.offer.features.includes(facilitiesArray[i].value);
-        });
-      }
+const checkFacilities = (estateObject) => { // проверка наличия в estateObject-те выбранных удобств
+  const facilitiesArray = document.querySelectorAll('#housing-features input'); // массив опций удобств
+  let indicator = true;
+  for (let i = 0; i < facilitiesArray.length; i++) { // цикл проходит по всем элементам массива опций удобств
+    if (facilitiesArray[i].checked && !estateObject.offer.features.includes(facilitiesArray[i].value)) { // если удобство чекнуто И его нет в estateObject-те, то...
+      indicator = false;
+      break;
     }
+  }
+  return indicator;
+};
 
-    const filteredOffers = facilitiesFilteredOffers; // создаётся массив окончательно отфильтрованных предложений
-
+const changeFiltersState = (cb, dataToFilter) => { // фильтрация данных с сервера
+  filterForm.addEventListener('change', () => {
+    const filteredOffers = dataToFilter.filter((estateObject) => {
+      return checkHousingType(estateObject) &&
+             checkPriceRange(estateObject) &&
+             checkHousingRooms(estateObject) &&
+             checkHousingGuests(estateObject) &&
+             checkFacilities(estateObject);
+    });
     cb(filteredOffers);
   });
 };
