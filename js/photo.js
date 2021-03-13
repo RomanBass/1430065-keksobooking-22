83e7698dad3d9f9feeb89becfd06038216fc95b4
Roman Katrovsky <katrovsky@gmail.com>
2021-03-13 11:53:17 +0300

    for checking

---
+++ js/photo.js
@@ -0,0 +1,47 @@
+import {form} from './form.js'
+
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+const avatarFileChooser = form.querySelector('.ad-form__field input[type=file]');
+const avatarPreview = form.querySelector('.ad-form-header__preview img');
+const housingFileChooser = form.querySelector('.ad-form__upload input[type=file]');
+const housingPreviewBlock = form.querySelector('.ad-form__photo');
+
+avatarFileChooser.addEventListener('change', function () {
+
+  const file = avatarFileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some(function (ending) {
+    return fileName.endsWith(ending);
+  });
+
+  if (matches) {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', function () {
+      avatarPreview.src = reader.result;
+    });
+    reader.readAsDataURL(file);
+  }
+
+});
+
+housingFileChooser.addEventListener('change', function () {
+
+  const file = housingFileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some(function (ending) {
+    return fileName.endsWith(ending);
+  });
+
+  if (matches) {
+    let reader = new FileReader();
+
+    reader.addEventListener('load', function () {
+      housingPreviewBlock.innerHTML = `<img src="${reader.result}" alt="Фото жилья" width="70" height="70"></img>`;
+    });
+    reader.readAsDataURL(file);
+  }
+
+});
